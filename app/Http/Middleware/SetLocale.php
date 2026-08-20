99054a8cd0943059e68f269e3d2e132940a058ee
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-08-20 14:46:05 +0300

    Add session based locale middleware

---
+++ app/Http/Middleware/SetLocale.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Http\Middleware;
+
+use Closure;
+
+class SetLocale
+{
+    public function handle($request, Closure $next)
+    {
+        $locale = session('locale', config('app.locale','ru'));
+        if (!in_array($locale,['ru','cv','mhr'],true)) {
+            $locale = 'ru';
+        }
+        app()->setLocale($locale);
+        return $next($request);
+    }
+}
