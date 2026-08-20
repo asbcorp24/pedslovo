d372db006db86f6b981436c04fe0b52fc5b47733
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-08-20 15:05:59 +0300

    Localize material page

--- resources/views/material.blade.php
+++ resources/views/material.blade.php
@@ -7,14 +7,14 @@
     <span class="badge text-bg-light">{{ $material->material_type }}</span>
     <h1 class="display-6 fw-bold mt-3">{{ $material->title }}</h1>
     @if($material->author)
-        <div class="text-muted mb-3">Автор: {{ $material->author }}</div>
+        <div class="text-muted mb-3">{{ __('Автор:') }} {{ $material->author }}</div>
     @endif
     @if($material->annotation)
         <p class="lead">{{ $material->annotation }}</p>
     @endif
     <div class="bg-white rounded-4 shadow-sm p-4">{!! $material->content !!}</div>
     @if($material->media_url)
-        <a class="btn btn-dark mt-3" href="{{ $material->media_url }}" target="_blank" rel="noopener">Открыть материал</a>
+        <a class="btn btn-dark mt-3" href="{{ $material->media_url }}" target="_blank" rel="noopener">{{ __('Открыть материал') }}</a>
     @endif
 </div></article>
 @endsection
