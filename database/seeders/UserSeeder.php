b982b953c1ab671aec2c953405a3caa321b93301
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-08-20 11:08:09 +0300

    Add base users seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'admin@pedslovo.local'],
+            ['name' => 'Администратор', 'password' => Hash::make('ChangeMe123!'), 'role' => 'admin']
+        );
+
+        User::updateOrCreate(
+            ['email' => 'teacher@pedslovo.local'],
+            ['name' => 'Преподаватель', 'password' => Hash::make('ChangeMe123!'), 'role' => 'teacher']
+        );
+
+        User::updateOrCreate(
+            ['email' => 'student@pedslovo.local'],
+            ['name' => 'Студент', 'password' => Hash::make('ChangeMe123!'), 'role' => 'student']
+        );
+    }
+}
